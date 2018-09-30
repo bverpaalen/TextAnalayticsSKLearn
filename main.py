03f1e9c994643c5d77442e450528279a574df479
funkychukie <brentverpaalen95@hotmail.com>
2018-09-30 20:13:42 +0200

    testing

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from sklearn.datasets import fetch_20newsgroups
 
-
+TEST = True
 
 def main():
     print("Starting data sklearn data text analaytics.")
-    set = retrieveData(1)
-    print(len(set.data))
+    print()
+    set = RetrieveData(1)
 
-def retrieveData(selector):
+    if TEST:
+        TestingSet(set)
+
+def RetrieveData(selector):
     if selector == 1:
         categories = ["alt.atheism", "soc.religion.christian", "comp.graphics", "sci.med"]
         twenty_Train = fetch_20newsgroups(subset="train",categories=categories,shuffle=True, random_state=42)
         return twenty_Train
     return None
+
+def TestingSet(set):
+    data = set.data
+    print("TESTING DATA")
+    print("Data size: "+str(len(data)))
+    print()
+    print("Targets: "+str(set.target_names))
+    print()
+    print("First lines first file:")
+    print("\n".join(data[0].split("\n")[:3]))
+    print()
 
 main()
