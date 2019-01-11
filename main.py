afecf05965003df9b80d35b5f89a6d925c120c26
bverpaalen <brentverpaalen95@hotmail.com>
2019-01-11 11:46:38 +0100

    version 1.0

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer as TTransformer
+from sklearn.naive_bayes import MultinomialNB as MNB
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier as SGDC
+from sklearn.tree import DecisionTreeClassifier as DTC
+from sklearn import metrics
+import numpy as np
 
-TEST = True
+TEST = False
+#categories = ["alt.atheism", "soc.religion.christian", "comp.graphics", "sci.med"]
+categories = None
+
+classifiers = [
+    MNB(),
+    DTC(),
+    SGDC()]
+fw = [
+     ('vect',CountVectorizer()), 
+      #("tf",TTransformer(use_idf=False)),
+      ("tfidf",TTransformer())]
 
 def main():
     print("Starting data sklearn data text analaytics.")
@@ -10,11 +27,22 @@ def main():
 
     if TEST:
         TestingSet(set)
-    WordVector(set.data)
+
+    for k in range(len(classifiers)):
+       clas = classifiers[k] 
+       print()
+       print("clf: "+str(clas))
+       for i in range(2,3):
+          print(i)
+          pip = fw[:i]
+          pip.append(('clf',clas))
+          print("Pipeline: "+str(pip))
+          clf = Pipeline(pip)
+          clf.fit(set.data,set.target)
+          evaluation(clf)
 
 def RetrieveData(selector):
     if selector == 1:
-        categories = ["alt.atheism", "soc.religion.christian", "comp.graphics", "sci.med"]
         twenty_Train = fetch_20newsgroups(subset="train",categories=categories,shuffle=True, random_state=42)
         return twenty_Train
     return None
@@ -34,6 +62,32 @@ def WordVector(data):
     count_vect = CountVectorizer()
     X_train_counts = count_vect.fit_transform(data)
     print(X_train_counts.shape)
-    print(count_vect.vocabulary_.get(u'algorithm')
+    print(count_vect.vocabulary_.get(u'algorithm'))
+    return count_vect
+
+def OcToFr(data,CV,target,names):
+    tf_transformer = TTransformer(use_idf=False).fit(data)
+    X_train_tf = tf_transformer.transform(data)
+    print(X_train_tf.shape)
+    ttransformer = TTransformer()
+    X_train_tfidf = ttransformer.fit_transform(data)
+
+    Xtrain = X_train_tfidf    
+    clf = MNB().fit(Xtrain,target)
+    docs_new = ['God is love','OpenGL in the GPU is fast']
+    X_new_counts = CV.transform(docs_new)
+    X_new_tfidf = ttransformer.transform(X_new_counts)
+    predicted = clf.predict(X_new_tfidf)
+
+    for doc, category in zip(docs_new, predicted):
+        print('%r => %s' % (doc, names[category]))
+
+def evaluation(text_clf):
+     twenty_test = fetch_20newsgroups(subset='test',
+        categories=categories, shuffle=True, random_state=42)
+     docs_test = twenty_test.data
+     predicted = text_clf.predict(docs_test)
+     print(np.mean(predicted == twenty_test.target))            
+     print(metrics.classification_report(twenty_test.target, predicted,target_names=twenty_test.target_names))
 
 main()
